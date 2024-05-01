37fadda88e417961ef3fd2144bf56d5b96a85e65
BLO-38 <lars3838@gmail.com>
2024-05-02 00:41:26 +0200

    Ny versionsnummer

--- src/model/MainHandler.java
+++ src/model/MainHandler.java
@@ -7,13 +7,16 @@ import view.*;
 
 public class MainHandler {
 	// Den 9 april 2024
-	public static final String version = "BLO 5.0";
+	public static final String version = "BLO 6.0";
 	// Vers 4:
 	// Radera klassrum
 	// Teacher view
 
 	// Vers 5:
 	// Total databese make over!
+	// Vers 6 den 1 maj 2024
+	// Korridorknappar, gruppindelning, offlinekontrollen
+
 	public static final Color MY_GREEN = new Color(27, 104, 5);
 	public static final Color MY_RED = new Color(0x950606);
 	public static final Color MY_BEIGE = new Color(0xE5D496);
